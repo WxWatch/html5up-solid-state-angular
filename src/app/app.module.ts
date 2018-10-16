8440810a24c944e6265c1fbd3ec4f5d72f49a927
James Glenn <jglenn1@gmail.com>
2018-10-15 19:13:46 -0500

    split out a bunch of stuff into components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { BannerComponent } from './banner/banner.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    BannerComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
